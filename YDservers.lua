9a15a0cac218174c47ba0162c67e1cccb5a50007
yaD5 <56451679+yaD5@users.noreply.github.com>
2024-03-13 12:35:07 +0300

    Update YDservers.lua

--- YDservers.lua
+++ YDservers.lua
@@ -19,7 +19,7 @@ local coordinates
 local notifs = loadstring(game:HttpGet('https://raw.githubusercontent.com/CF-Trail/random/main/FE2Notifs.lua'))()
 
 notifs.alert('Execute "_G.s = false" to stop!\nThis is the fastest it can go due to stairs being generated.', nil, 1000000, 'rainbow')
-task.wait(0.7)
+task.wait(0.57)
 
 local lastNotificationTime = 0
 local notificationDelay = 0.5
@@ -46,7 +46,7 @@ task.wait(5)
 
 game.Players.LocalPlayer.Character.HumanoidRootPart.CFrame = CFrame.new(630.6519165039062, 143.7024383544922, -1891.4598388671875)
 
-task.wait(0.7)
+task.wait(0.57)
 
 _G.s = true
 local function updateYCoordinate()
